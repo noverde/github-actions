5d7fd32827708d444a19896522a4f9d4c01628bd
Everaldo Canuto <everaldo.canuto@gmail.com>
2021-03-18 16:37:13 -0300

    Remove action output

--- aws-configure/action.sh
+++ aws-configure/action.sh
@@ -12,5 +12,3 @@ cat >> ~/.aws/credentials <<-EOF
     aws_access_key_id=$AWS_ACCESS_KEY_ID
     aws_secret_access_key=$AWS_SECRET_ACCESS_KEY
 EOF
-
-echo "::set-output name=random-id::$(echo $AWS_DEFAULT_REGION)"
